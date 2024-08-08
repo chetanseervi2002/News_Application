6b3e9d8f63b405406c27ddeda064d9b5a3c4e531
Chetan Pukhraj Choudhary <21.csechetan@mitsjadan.ac.in>
2024-08-08 07:17:54 +0530

    Final Code

---
+++ tempCodeRunnerFile.c
@@ -0,0 +1,17 @@
+/*
+        AUTHOR:CHETAN CHOUDHARY
+        DATE  :
+        TOPIC :
+*/
+
+#include<stdio.h>
+#include<conio.h>
+
+int main()
+{
+    if(printf("Hello")){
+        
+    }
+
+    return 0;
+}
